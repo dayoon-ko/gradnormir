99267fa889964590581293be178c50d3a6814a3e
dayoon-ko <102837976+dayoon-ko@users.noreply.github.com>
2025-05-31 16:45:41 +0900

    Delete finetune directory

--- finetune/retrieval/retriever.sh
+++
@@ -1,9 +0,0 @@
-data_root="eval_retrieval/retrieval/datasets"
-dataset_name="scifact"
-model_name="/gallery_louvre/sohyeon.kim/eval_retrieval/finetune/train/checkpoint-167"
-
-python retriever.py \
-   --dataset_name ${dataset_name} \
-   --save_root "/gallery_louvre/sohyeon.kim/eval_retrieval/finetune/retrieval/results" \
-   --db_faiss_dir vectorstore/${dataset_name}/ \
-   --model_name ${model_name} \
